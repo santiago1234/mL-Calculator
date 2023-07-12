54c28ec466db9d87984fe2b83d8ba3db60c0a78d
Santiago Medina <santiagogmm@hotmail.com>
2023-07-12 12:57:28 -0500

    better code to get the coding intervals

--- example_data/get-coding-intervals-per-gene.R
+++ example_data/get-coding-intervals-per-gene.R
@@ -1,34 +1,48 @@
 # Load the library
 # Get the exon intervals of a random gene for testing the pipeline
 library(biomaRt)
-library(magrittr)
 library(GenomicRanges)
 library(rtracklayer)
-library(magrittr)
 library(parallel)
+library(tidyverse)
 # Select the dataset
 ensembl <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 
+genes <- read_csv('protein_coding_genes.csv')
 
-get_coding_intervals <- function(gene_id) {
-  # Get coding sequence coordinates
-  coding_intervals <- getBM(attributes = c('chromosome_name', 'exon_chrom_start', 'exon_chrom_end'),
-                            filters = 'ensembl_gene_id',
-                            values = gene_id, 
-                            mart = ensembl)
+genes <- genes$`Gene stable ID`
+
+coding_intervals <- getBM(attributes = c('ensembl_gene_id', 'chromosome_name', 'exon_chrom_start', 'exon_chrom_end'),
+                          filters = 'ensembl_gene_id',
+                          values = genes, 
+                          mart = ensembl)
+
+coding_intervals <- 
+  coding_intervals %>% 
+  as_tibble() %>% 
+  mutate(
+    grp_gene = ensembl_gene_id
+  ) %>% 
+  group_by(grp_gene) %>% 
+  nest()
+
+get_interval_gene <- function(gene_data) {
   
-  # Create a GRanges object from the data frame
-  gr <- makeGRangesFromDataFrame(coding_intervals,
+  
+  gene_id <- gene_data$ensembl_gene_id[1]
+  outfile <- paste0('coding-intervals/', gene_id, '.bed')
+  
+  gr <- makeGRangesFromDataFrame(gene_data,
                                  keep.extra.columns=TRUE,
                                  start.field="exon_chrom_start",
                                  end.field="exon_chrom_end",
-                                 ignore.strand = T,
-                                 seqnames.field="chromosome_name") %>% 
+                                 ignore.strand = TRUE,
+                                 seqnames.field="chromosome_name") %>%
     IRanges::reduce()
   
-  # Export the GenomicRanges object to a BED file, including extra columns
-  export.bed(gr, con = paste0('coding-intervals/', gene_id, ".bed"))
+  export.bed(gr, con = outfile)
 }
 
-genes <- read.csv('protein_coding_genes.csv')
-mclapply(X = genes$Gene.stable.ID, FUN = get_coding_intervals, mc.cores = 6)
+
+coding_intervals$data %>% 
+  mclapply(get_interval_gene, mc.cores = 10)
